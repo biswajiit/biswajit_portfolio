f01a205ac846b2a670ec61d8e5aff0cf3b3347e6
biswajiit <biswajit.jmishra@gmail.com>
2018-07-31 22:48:45 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,6 @@ export class AppComponent {
   detail:String = "Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.";
   name:String = "Biswajit Mishra";
   gitLink:String = "https://github.com/biswajiit";
+  linkedInLink:String ="https://www.linkedin.com/in/biswajit-mishra-55060867/";
+  linkedInLink_short:String = "https://bit.ly/2v67CaB";
 }
