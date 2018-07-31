dccfef25345808cffa257d5496dd4e1553cf6529
biswajiit <biswajit.jmishra@gmail.com>
2018-07-31 22:33:41 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   detail:String = "Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.";
-  name:String = "Biswajit MIshra"
+  name:String = "Biswajit Mishra";
+  gitLink:String = "https://github.com/biswajiit";
 }
